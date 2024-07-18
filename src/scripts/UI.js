093f1f019c26e4778f21586a7f54bee944d703ea
nk-on <johndoe@example.com>
2024-07-18 22:23:55 +0400

    Refactor:moving code which appends sqaures inside image container to function

--- src/scripts/UI.js
+++ src/scripts/UI.js
@@ -1,6 +1,9 @@
-import { imageContainer } from "./script.js";
-for (let i = 1; i <= 16; i++) {
-  const square = document.createElement('div');
-  square.classList.add('square');
-  imageContainer.appendChild(square);
-}
+import { imageContainer } from './script.js';
+function appendSquares() {
+  for (let i = 1; i <= 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    imageContainer.appendChild(square);
+  }
+};
+appendSquares();
