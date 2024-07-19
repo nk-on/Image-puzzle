9d15a6758029d12bb6930aeea067cb538529b8b5
nk-on <johndoe@example.com>
2024-07-19 22:02:47 +0400

    Moving appendslides function to scripts.js file

--- src/scripts/UI.js
+++ src/scripts/UI.js
@@ -6,21 +6,4 @@ function appendSquares() {
     imageContainer.appendChild(square);
   }
 }
-function appendSlides() {
-  let i = 0;
-  const numbers = new Set();
-  while (i < 16) {
-    let randIdx = Math.floor(Math.random() * 16);
-    if (numbers.has(randIdx)) {
-      randIdx = Math.floor(Math.random() * 16);
-    } else {
-      const slide = document.createElement('img');
-      slide.classList.add('slide');
-      slide.setAttribute('src', `Assets/toucan-splitedImages/${randIdx}.jpg`);
-      slidesContainer.appendChild(slide);
-      i++;
-    }
-  }
-}
 appendSquares();
-appendSlides();
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,20 @@
 const slidesContainer = document.querySelector('[data-container="slides"]');
 const imageContainer = document.querySelector('[data-container="image"]');
-
-export {imageContainer,slidesContainer}
+function appendSlides() {
+  let i = 0;
+  const numbers = new Set();
+  while (i < 16) {
+    let randIdx = Math.floor(Math.random() * 16);
+    if (numbers.has(randIdx)) {
+      randIdx = Math.floor(Math.random() * 16);
+    } else {
+      const slide = document.createElement('img');
+      slide.classList.add('slide');
+      slide.setAttribute('src', `Assets/toucan-splitedImages/${randIdx}.jpg`);
+      slidesContainer.appendChild(slide);
+      i++;
+    }
+  }
+};
+appendSlides();
+export { imageContainer, slidesContainer };
