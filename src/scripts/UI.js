15e4e2c7c39cb2725892c2de7360b91c77eb3f7f
nk-on <johndoe@example.com>
2024-07-21 12:33:20 +0400

    Deleted separete UI.js file

--- src/scripts/UI.js
+++
@@ -1,9 +0,0 @@
-import { imageContainer, slidesContainer } from './script.js';
-function appendSquares() {
-  for (let i = 1; i <= 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    imageContainer.appendChild(square);
-  }
-}
-appendSquares();
